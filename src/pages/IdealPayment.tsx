69b63696e98615511bec608e844bd8995f1dfbfe
klaas1717 <klaasvalentijn1984@gmail.com>
2026-09-14 19:26:46 +0200

    update idealpayment.tsx2

--- src/pages/IdealPayment.tsx
+++ src/pages/IdealPayment.tsx
@@ -9,6 +9,26 @@ const PRIMARY = '#CC0066';
 const BG = '#EEF5F7';
 const FONT = '"Lexend Deca", sans-serif';
 const HEAD = '"Roboto Slab", serif';
+const BANKS = [
+  'ABN AMRO',
+  'Adyen',
+  'ASN Bank',
+  'ASN Bank vh RegioBank',
+  'ASN Bank voorheen SNS',
+  'bunq',
+  'BUUT',
+  'Finom',
+  'ING',
+  'Knab',
+  'Mollie',
+  'N26',
+  'Nationale-Nederlanden',
+  'Rabobank',
+  'Revolut',
+  'Triodos Bank',
+  'Van Lanschot Kempen',
+  'Yoursafe',
+];
 
 
 function formatAmount(value: string | null) {
